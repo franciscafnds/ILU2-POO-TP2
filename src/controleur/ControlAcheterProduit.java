3d32ad6c55754da91ef1a60c2d2a5518c0b63baa
franciscafnds <francisca.fernandes-gomes-de-almeida@utoulouse.fr>
2025-11-13 08:07:48 +0100

    fin tp2/3

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -1,5 +1,7 @@
 package controleur;
 
+import personnages.Gaulois;
+import villagegaulois.Etal;
 import villagegaulois.Village;
 
 public class ControlAcheterProduit {
@@ -15,6 +17,28 @@ public class ControlAcheterProduit {
 		this.controlTrouverEtalVendeur = controlTrouverEtalVendeur;
 	}
 	
-	// TODO à complete
+	public int acheterProduit(String nom, int nbProduitAchete) {
+		Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nom);
+		return etal.acheterProduit(nbProduitAchete);
+	}	
 	
+	public boolean isHabitant(String nom) {
+		return controlVerifierIdentite.verifierIdentite(nom);
+	}
+	
+	public Etal[] rechercherEtalsProduit(String produit) {
+		Gaulois[] vendeurProduit = village.rechercherVendeursProduit(produit);
+		if (vendeurProduit == null) {
+			return new Etal[0];
+		} else {
+			Etal[] etals = new Etal[vendeurProduit.length];
+			for (int i = 0; i<vendeurProduit.length; i++) {
+				etals[i] = controlTrouverEtalVendeur.trouverEtalVendeur(vendeurProduit[i].getNom());
+			}
+			return etals;
+		}
+		
+	}
 }
+	
+
--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -1,6 +1,7 @@
 package frontiere;
 
 import controleur.ControlAcheterProduit;
+import villagegaulois.Etal;
 
 public class BoundaryAcheterProduit {
 	private ControlAcheterProduit controlAcheterProduit;
@@ -10,6 +11,52 @@ public class BoundaryAcheterProduit {
 	}
 
 	public void acheterProduit(String nomAcheteur) {
-		// TODO à completer
+        
+	    if (controlAcheterProduit.isHabitant(nomAcheteur)) {
+	        String produit = Clavier.entrerChaine("Quel produit voulez vous acheter ?");
+
+	        Etal[] etals = controlAcheterProduit.rechercherEtalsProduit(produit);
+	        if (etals.length == 0) {
+	            System.out.println("Désolé, personne ne vend ce produit au marché\n");
+	            return;
+	        }
+
+	        for (int i = 0; i < etals.length; i++) {
+	            System.out.println((i + 1) + " - " + etals[i].getVendeur().getNom());
+	        }
+
+	        int vendeur = Clavier.entrerEntier("Choisissez un vendeur valide :");
+	        while (vendeur <= 0 || vendeur > etals.length) {
+	            vendeur = Clavier.entrerEntier("Choisissez un vendeur valide :");
+	        }
+	        vendeur -= 1;
+
+	        System.out.println(nomAcheteur + " se déplace jusqu'à l'étal du vendeur " 
+	            + etals[vendeur].getVendeur().getNom() 
+	            + "\nBonjour " + nomAcheteur);
+	        
+	        int nombreAchete = Clavier.entrerEntier("Combien de " + produit + " voulez-vous acheter ?");
+
+	        int quantiteRestante = etals[vendeur].getQuantite();
+	        if (nombreAchete > quantiteRestante && quantiteRestante != 0) {
+	            System.out.println(nomAcheteur + " veut acheter " + nombreAchete + " " + produit 
+	                + ", malheureusement " + etals[vendeur].getVendeur().getNom() 
+	                + " n'en a plus que " + quantiteRestante + ". " 
+	                + nomAcheteur + " achète tout le stock de " 
+	                + etals[vendeur].getVendeur().getNom() + ".");
+	        } else if (quantiteRestante == 0) {
+	            System.out.println(nomAcheteur + " veut acheter " + nombreAchete + " " + produit 
+	                + ", malheureusement il n'y en a plus !");
+	        } else {
+	            System.out.println(nomAcheteur + " achète " + nombreAchete + " " + produit 
+	                + " à " + etals[vendeur].getVendeur().getNom() + ".");
+	        }
+
+	        controlAcheterProduit.acheterProduit(etals[vendeur].getVendeur().getNom(), nombreAchete);
+
+	    } else {
+	        System.out.println("Je suis désolé " + nomAcheteur 
+	            + " mais il faut être un habitant de notre village pour commercer ici.");
+	    }
 	}
 }
